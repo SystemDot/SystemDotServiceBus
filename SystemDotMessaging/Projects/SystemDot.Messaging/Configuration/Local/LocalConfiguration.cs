5e36906ed01c93530369590851b1902a1c0c5162
sammosampson <sammosampson@gmail.com>
2012-06-17 12:59:11 +0100

    Combined config into messaging project

---
+++ SystemDotMessaging/Projects/SystemDot.Messaging/Configuration/Local/LocalConfiguration.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics.Contracts;
+using System.Runtime.Serialization;
+using SystemDot.Messaging.Configuration.Remote;
+using SystemDot.Threading;
+
+namespace SystemDot.Messaging.Configuration.Local
+{
+    public class LocalConfiguration
+    {
+        readonly ThreadPool threadPool;
+        
+        public LocalConfiguration(ThreadPool threadPool)
+        {
+            Contract.Requires(threadPool != null);
+
+            this.threadPool = threadPool;
+        }
+
+        public UsingDefaultsConfiguration UsingDefaults()
+        {
+            return new UsingDefaultsConfiguration(this.threadPool);
+        }
+    }
+}
---
+++ SystemDotMessaging/Projects/SystemDot.Messaging/Configuration/Remote/RemoteConfiguration.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics.Contracts;
+using SystemDot.Threading;
+
+namespace SystemDot.Messaging.Configuration.Remote
+{
+    public class RemoteConfiguration
+    {
+        readonly ThreadedWorkCoordinator workCoordinator;
+        readonly ThreadPool threadPool;
+
+        public RemoteConfiguration(ThreadedWorkCoordinator workCoordinator, ThreadPool threadPool)
+        {
+            Contract.Requires(workCoordinator != null);
+            Contract.Requires(threadPool != null);
+            
+            this.workCoordinator = workCoordinator;
+            this.threadPool = threadPool;
+        }
+
+        public UsingDefaultsConfiguration UsingDefaults()
+        {
+            return new UsingDefaultsConfiguration(workCoordinator, threadPool);    
+        }
+    }
+}
