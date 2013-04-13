7da80133d2fdf886354f268d5c21ca58f9490b58
sammosampson <sammosampson@gmail.com>
2013-04-13 08:28:53 +0100

    Added new queue that uses producer consumer pattern

--- SystemDotMessaging/Projects/SystemDot.Messaging.Specifications/channels/point_to_point/repeating/escalating/http/when_repeating_a_message_and_four_seconds_have_passed.cs
+++ SystemDotMessaging/Projects/SystemDot.Messaging.Specifications/channels/point_to_point/repeating/escalating/http/when_repeating_a_message_and_four_seconds_have_passed.cs
@@ -1,49 +1,49 @@
 using System;
+using SystemDot.Http;
 using SystemDot.Http.Builders;
-using SystemDot.Messaging.Packaging;
-using SystemDot.Messaging.Specifications.channels;
-using SystemDot.Messaging.Specifications.channels.point_to_point.repeating.escalating;
-using SystemDot.Messaging.Transport;
-using SystemDot.Messaging.Transport.InProcess;
 using SystemDot.Parallelism;
+using SystemDot.Serialisation;
 using SystemDot.Specifications;
 using Machine.Specifications;
 using SystemDot.Messaging.Transport.Http.Configuration;
 
-namespace SystemDot.Messaging.Specifications.transport.http
+namespace SystemDot.Messaging.Specifications.channels.point_to_point.repeating.escalating.http
 {
     [Subject(SpecificationGroup.Description)]
     public class when_repeating_a_message_and_four_seconds_have_passed : WithMessageConfigurationSubject
     {
         const string ChannelName = "Test";
         const string SenderChannelName = "TestSender";
+        const string ServerInstance = "ServerInstance";
 
-        static IBus bus;
-        static int message;
-        static TestSystemTime systemTime;
-
+        static TestWebRequestor webRequestor;
+        
         Establish context = () =>
         {
             ConfigureAndRegister<IHttpServerBuilder>(new TestHttpServerBuilder());
-            
-            systemTime = new TestSystemTime(DateTime.Now);
+
+            webRequestor = new TestWebRequestor(
+                new PlatformAgnosticSerialiser(),
+                new FixedPortAddress(Environment.MachineName, ServerInstance));
+
+            ConfigureAndRegister<IWebRequestor>(webRequestor);
+
+            var systemTime = new TestSystemTime(DateTime.Now);
             ConfigureAndRegister<ISystemTime>(systemTime);
 
-            bus = Configuration.Configure.Messaging()
-                .UsingHttpTransport().AsAServer("ServerInstance")
+            IBus bus = Configuration.Configure.Messaging()
+                .UsingHttpTransport().AsAServer(ServerInstance)
                 .OpenChannel(ChannelName)
                 .ForPointToPointSendingTo(SenderChannelName)
                 .Initialise();
 
-            message = 1;
-
-            bus.Send(message);
+            bus.Send(1);
 
             systemTime.AddToCurrentDate(TimeSpan.FromSeconds(4));
         };
 
         Because of = () => The<ITaskRepeater>().Start();
 
-        It should_repeat_the_message = () => Server.SentMessages.Count.ShouldEqual(2);
+        It should_repeat_the_message = () => webRequestor.RequestsMade.Count.ShouldEqual(2);
     }
 }
