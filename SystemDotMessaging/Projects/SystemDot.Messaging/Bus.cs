d9537cafa7b06d250c50e6dfc0b25e9bfff9aba9
samprakash <sammosampson@gmail.com>
2013-07-13 21:47:40 +0100

    Added logging to bus

--- SystemDotMessaging/Projects/SystemDot.Messaging/Bus.cs
+++ SystemDotMessaging/Projects/SystemDot.Messaging/Bus.cs
@@ -1,4 +1,5 @@
 using SystemDot.Ioc;
+using SystemDot.Logging;
 using SystemDot.Messaging.Batching;
 using SystemDot.Messaging.Ioc;
 
@@ -8,21 +9,25 @@ namespace SystemDot.Messaging
     {
         public static void Send(object message)
         {
+            Logger.Info("Sending message: {0}", message.GetType().Name);
             GetBus().Send(message);
         }
 
         public static void SendLocal(object message)
         {
+            Logger.Info("Sending local message: {0}", message.GetType().Name);
             GetBus().SendLocal(message);
         }
 
         public static void Reply(object message)
         {
+            Logger.Info("Replying with message: {0}", message.GetType().Name);
             GetBus().Reply(message);
         }
 
         public static void Publish(object message)
         {
+            Logger.Info("Publishing message: {0}", message.GetType().Name);
             GetBus().Publish(message);
         }
 
