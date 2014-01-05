28425ee81e6c6ac517a2bf993a766ff3273c5cd3
sammosampson <sammosampson@gmail.com>
2014-01-05 09:54:53 +0000

    Changed other direct channel example to use traditional handler style

--- SystemDotMessaging/Projects/Examples/Windows/OtherDirectRequestReplySender/Program.cs
+++ SystemDotMessaging/Projects/Examples/Windows/OtherDirectRequestReplySender/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using SystemDot.Ioc;
 using SystemDot.Logging;
+using SystemDot.Messaging;
 using SystemDot.Messaging.Configuration;
+using Messages;
 
 namespace OtherDirectRequestReplySender
 {
@@ -8,23 +11,33 @@ namespace OtherDirectRequestReplySender
     {
         static void Main(string[] args)
         {
+            var container = new IocContainer();
+            container.RegisterFromAssemblyOf<Program>();
+
             Configure.Messaging()
-                .LoggingWith(new ConsoleLoggingMechanism { ShowInfo = false, ShowDebug = false })
+                .LoggingWith(new ConsoleLoggingMechanism {ShowInfo = false, ShowDebug = false})
+                .ResolveReferencesWith(container)
+                .RegisterHandlersFromAssemblyOf<Program>().BasedOn<IMessageConsumer>()
                 .UsingHttpTransport()
                     .AsAServer("OtherSenderServer")
                 .OpenDirectChannel("OtherTestRequest")
                     .ForRequestReplySendingTo("TestReply@ReceiverServer")
                 .Initialise();
 
-            var sender = new DirectSender();
-            
             do
             {
                 Console.WriteLine("I am the other sender. Press enter to send messages..");
                 Console.ReadLine();
                 Console.WriteLine("Sending messages");
 
-                sender.Send();
+                Bus.SendDirect(new TestMessage("Hello"));
+                Bus.SendDirect(new TestMessage("Hello1"));
+                Bus.SendDirect(new TestMessage("Hello2"));
+                Bus.SendDirect(new TestMessage("Hello3"));
+                Bus.SendDirect(new TestMessage("Hello4"));
+                Bus.SendDirect(new TestMessage("Hello5"));
+                Bus.SendDirect(new TestMessage("Hello6"));
+                Bus.SendDirect(new TestMessage("Hello7")); 
             }
             while (true);
         }
